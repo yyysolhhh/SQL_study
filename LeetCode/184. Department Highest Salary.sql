0a1f468b69d539538e8d3a4d677f5a95c61187ba
yyysolhhh <solbaram37@gmail.com>
2022-04-24 03:51:19 +0900

    220424 sql subquery

--- LeetCode/184. Department Highest Salary.sql
+++ LeetCode/184. Department Highest Salary.sql
@@ -1,8 +1,8 @@
 SELECT D.name AS Department, E.name AS Employee, E.salary AS Salary 
 FROM Employee E JOIN Department D 
 ON E.departmentId = D.id 
-WHERE Salary IN 
-	(SELECT MAX(salary) 
+WHERE (E.departmentId, Salary) IN 
+	(SELECT departmentId, MAX(salary) 
 	FROM Employee 
 	GROUP BY departmentId)
 ;
