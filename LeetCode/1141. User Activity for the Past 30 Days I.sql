389fe6ab708d85b28a8d9396bbb7513a3cc53ecd
yyysolhhh <solbaram37@gmail.com>
2022-04-27 16:10:02 +0900

    220427 sql datediff

--- LeetCode/1141. User Activity for the Past 30 Days I.sql
+++ LeetCode/1141. User Activity for the Past 30 Days I.sql
@@ -2,3 +2,9 @@ SELECT activity_date AS day, COUNT(DISTINCT(user_id)) AS active_users
 FROM Activity 
 WHERE activity_date >= '2019-06-28' AND activity_date <= '2019-07-27' 
 GROUP BY activity_date;
+
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users 
+FROM Activity 
+WHERE DATEDIFF('2019-07-27', activity_date) < 30 
+GROUP BY activity_date;
+
