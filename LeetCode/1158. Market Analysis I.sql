b5e5b11b075b1be625871a3431b48250f818bfc7
yyysolhhh <solbaram37@gmail.com>
2022-05-15 21:07:25 +0900

    220515 sql solving

---
+++ LeetCode/1158. Market Analysis I.sql
@@ -0,0 +1,1 @@
+SELECT U.user_id AS buyer_id, U.join_date, IF(ISNULL(O.order_date), 0, COUNT(O.order_id)) AS orders_in_2019 FROM Orders O RIGHT JOIN Users U ON O.buyer_id = U.user_id WHERE YEAR(O.order_date) = 2019 GROUP BY U.user_id;
