cca972b27b4f68af51fca792311f332432b8ed25
yyysolhhh <solbaram37@gmail.com>
2022-05-18 00:23:29 +0900

    220519 sql solving

--- LeetCode/1393. Capital Gain Loss.sql
+++ LeetCode/1393. Capital Gain Loss.sql
@@ -1,1 +1,1 @@
-SELECT stock_name, SUM() AS capital_gain_loss FROM Stocks GROUP BY stock_name;
+SELECT stock_name, SUM(CASE operation WHEN 'Buy' THEN -1 * price WHEN 'Sell' THEN price END) AS capital_gain_loss FROM Stocks GROUP BY stock_name;
